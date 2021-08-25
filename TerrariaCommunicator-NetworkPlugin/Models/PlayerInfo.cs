6141aa6d6176fe62d1c58ab51a6c754be7ebe88d
AuriRex <xaurirexx@gmail.com>
2021-08-25 04:25:02 +0200

    ChatMessagePacket

---
+++ TerrariaCommunicator-NetworkPlugin/Models/PlayerInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerrariaCommunicator_NetworkPlugin.Models
+{
+    public class PlayerInfo
+    {
+        public string Name { get; set; }
+        public string UUID { get; set; }
+        public bool IsDead { get; set; }
+    }
+}
---
+++ TerrariaCommunicator-NetworkPlugin/Packets/ChatMessagePacket.cs
@@ -0,0 +1,16 @@
+﻿using Communicator.Packets;
+using TerrariaCommunicator_NetworkPlugin.Models;
+
+namespace TerrariaCommunicator_NetworkPlugin.Packets
+{
+    public class ChatMessagePacket : BasePacket<ChatMessagePacket.Content>
+    {
+        public override Content PacketData { get; set; }
+
+        public struct Content
+        {
+            public string Message { get; set; }
+            public PlayerInfo PlayerInfo { get; set; }
+        }
+    }
+}
